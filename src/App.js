f90d01100a6fe415e96145faa8e4cafdcd61d787
WycMB <wyc0914@gmail.com>
2023-03-26 00:20:30 -0400

    added card matching function

--- src/App.js
+++ src/App.js
@@ -3,15 +3,19 @@ import Cards from './Cards.js';
 import { useState } from 'react';
 
 function ShuffleCard(cards) {
-  const ShuffleCard = [...cards]
-  ShuffleCard.sort(() => Math.random() - 0.5); 
+  let ShuffleCard = [];
+  while (cards.length) {
+    const randomIndex = Math.floor(Math.random() * cards.length);
+    ShuffleCard.push(cards[randomIndex]);
+    cards.splice(randomIndex, 1);
+  }
   return ShuffleCard;
 }
 
 function App() {
   const [cards, setcards] = useState(ShuffleCard([
-    {id: 1, cardback: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088876991526223882/CardBack.png', cardfront: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088274081004134530/image.png',  matched: false, flipped: false},
-    {id: 2, cardback: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088876991526223882/CardBack.png', cardfront: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088274081004134530/image.png',  matched: false, flipped: false},
+    {id: 1, cardback: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088876991526223882/CardBack.png', cardfront: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088274786620285018/image.png',  matched: false, flipped: false},
+    {id: 2, cardback: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088876991526223882/CardBack.png', cardfront: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088274786620285018/image.png',  matched: false, flipped: false},
     {id: 3, cardback: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088876991526223882/CardBack.png', cardfront: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088274151594266664/image.png',  matched: false, flipped: false},
     {id: 4, cardback: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088876991526223882/CardBack.png', cardfront: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088274151594266664/image.png',  matched: false, flipped: false},
     {id: 5, cardback: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088876991526223882/CardBack.png', cardfront: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088274246498787398/image.png',  matched: false, flipped: false},
@@ -23,11 +27,45 @@ function App() {
     {id: 11, cardback: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088876991526223882/CardBack.png', cardfront: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088274688268042377/image.png',  matched: false, flipped: false},
     {id: 12, cardback: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088876991526223882/CardBack.png', cardfront: 'https://cdn.discordapp.com/attachments/1077038540359221258/1088274688268042377/image.png',  matched: false, flipped: false},
   ]));
+  
+  //check the two selected cards for a matching cardfront, return true for a match and false otherwise
+  function isMatch(cardId1, cardId2, cards) {
+    const {cardfront: cardfront1} = cards.find(card => card.id === cardId1);
+    const {cardfront: cardfront2} = cards.find(card => card.id === cardId2);
+    return cardfront1 === cardfront2;
+  }
 
   const flipCard = (id) => {
     setcards((prevState) => prevState.map((card) => {
       return card.id === id ? { ...card, flipped: !card.flipped } : card;
     }));
+  
+    setcards((prevState) => {
+      const flippedCards = prevState.filter((card) => card.flipped && !card.matched);//filter out cards that is flipped and not matched
+      if (flippedCards.length === 2) {
+        if (isMatch(flippedCards[0].id, flippedCards[1].id, prevState)) {//check the two flipped cards are matched
+          return prevState.map((card) => {
+            if (card.id === flippedCards[0].id || card.id === flippedCards[1].id) {
+              return { ...card, matched: true, flipped: false };//change matched to true and flipped to false
+            } else {
+              return card;
+            }
+          });
+        } else {
+          //two selected cards are not a match
+          setTimeout(() => {
+            setcards((prevState) => prevState.map((card) => {
+              if (card.flipped && !card.matched) {
+                return { ...card, flipped: false };//set flipped to back to false if cards are not matched
+              } else {
+                return card;
+              }
+            }));
+          }, 700);//delay time
+        }
+      }
+      return prevState;
+    });
   };
 
   return (
@@ -38,12 +76,13 @@ function App() {
           <tbody>
             <tr>
               <td className="newGameButton">
-                <button className='button'>New Game</button>
+                <button className='button' onClick={ShuffleCard}>New Game</button>
               </td>
             </tr>
             <tr>
               <td className="attemptCounter">Attempts:</td>
             </tr>
+            <ul className="cards">
             <tr>
               <td>
                 {cards.slice(0, 4).map((card) => (
@@ -65,6 +104,7 @@ function App() {
                 ))}
               </td>
             </tr>
+            </ul>
           </tbody>
         </table>
       </header>
